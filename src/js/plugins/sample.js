e5f809aed2a1a8b99bbe50a9896ba049581ca895
Yusuke HIDESHIMA <hidesuke@gmail.com>
2018-01-14 23:56:10 +0900

    add 'weight' to plugin's output

--- src/js/plugins/sample.js
+++ src/js/plugins/sample.js
@@ -1,13 +1,13 @@
 const sample = {};
 
 sample.action = (input) => {
-  return fetch('http://zipcloud.ibsnet.co.jp/api/search?zipcode=2410821', {mode: 'cors'})
+  return fetch('http://zipcloud.ibsnet.co.jp/api/search?zipcode=2410821', { mode: 'cors' })
     .then(res => res.json())
     .then(json => {
       return JSON.stringify(json.results);
     })
     .then(str => {
-      return { text: str };
+      return { text: str, weight: 1 };
     });
 };
 
