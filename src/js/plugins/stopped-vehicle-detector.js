93e54b0a1e14e42cd1e8e8e5bb4b9d5992fe3e16
Yusuke HIDESHIMA <hidesuke@gmail.com>
2018-01-27 23:44:48 +0900

    停車中の返答にパターンを追加

--- src/js/plugins/stopped-vehicle-detector.js
+++ src/js/plugins/stopped-vehicle-detector.js
@@ -34,6 +34,48 @@ const outputs = [
       sadness: 1.0,
       anger: 0.0
     }
+  },
+  {
+    text: `ときには立ち止まることも大事。迷える子羊よ。`,
+    kuma: 'normal',
+    speech: {
+      text: `ときには立ち止まることも大事。迷える子羊よ。`,
+      volume: 1.0,
+      speed: 0.7,
+      pitch: 0.5,
+      range: 2.0,
+      joy: 0.2,
+      sadness: 0.2,
+      anger: 0.0
+    }
+  },
+  {
+    text: `車停まった瞬間にテンション上がることない！？　いまアゲアゲですよ！`,
+    kuma: 'love',
+    speech: {
+      text: `クルマ止まった瞬間にテンションあがることない！？　いまアゲアゲですよ！`,
+      volume: 1.0,
+      speed: 1.2,
+      pitch: 0.5,
+      range: 2.0,
+      joy: 1.0,
+      sadness: 0.0,
+      anger: 0.0
+    }
+  },
+  {
+    text: `ナイス停車！　バナナの革が落ちてるのよく気づいたよね`,
+    kuma: 'smile',
+    speech: {
+      text: `ナイス停車！　バナナの革が落ちてるのよく気づいたよね`,
+      volume: 1.0,
+      speed: 0.7,
+      pitch: 1.0,
+      range: 2.0,
+      joy: 1.0,
+      sadness: 0.0,
+      anger: 0.0
+    }
   }
 ];
 
